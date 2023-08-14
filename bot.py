a55a0bb1c89e86021379fb07323458dd92d7ff4f
Hugo Colson <hugo.colson@papernest.com>
2023-08-14 18:38:59 +0200

    First commit

---
+++ bot.py
@@ -0,0 +1,129 @@
+import discord
+import pandas as pd
+from royaleapi_scraping_class import ScrapingRoyaleAPI
+from discord import app_commands
+
+TOKEN = "MTEzOTk1NTAxOTc4NTk2MTU1Mg.GRziG1.okuLmjskQZ6S9WJLVRcfX-mvFgC6uWkiH_wh4g"
+SERVER_ID = 913101617639862362
+
+intents = discord.Intents.all()
+intents.members = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=SERVER_ID))
+    print("Logged in as Clash Royale Pinger!")
+    
+@tree.command(name="correspondances", description="Know which CR player is related to which discord id", guild=discord.Object(id=SERVER_ID))
+async def correspondances(ctx):
+    guild = client.get_guild(SERVER_ID)
+    members = guild.members
+    
+    user_info = []
+    
+    for member in members:
+        user_info.append({
+            "name": member.name,
+            "discord_id": member.id,
+            "nickname": member.nick,
+            "global_name": member.global_name,
+            "discord_name": member.nick or member.global_name or member.name,
+        })
+    
+    df_discord_data = pd.DataFrame(user_info)
+    
+    RoyaleAPI_scraper = ScrapingRoyaleAPI("UURJ9CG", df_discord_data)
+    
+    df_players_data = RoyaleAPI_scraper.run()
+    
+    df_players_data = df_players_data.astype(str)
+    
+    embed = discord.Embed(title="Discord & Clash Royale",description="Correspondances", colour=discord.Colour(0x3e038c))
+
+    dico_certainty = {"Confirmed":(1,0.9),"Uncertain":(0.9,0.5),"Bad":(0.5,0)}
+    
+    for certainty in dico_certainty.keys():
+        
+        (x_max, x_min) = dico_certainty[certainty]
+        
+        df_players_data["match_ratio"] = df_players_data["match_ratio"].astype(float)
+        
+        df_certainty = df_players_data.query(f'{x_max} >= match_ratio > {x_min}').sort_values(by='match_ratio', ascending=False)
+        
+        if len(df_certainty) >= 1:
+            
+            embed_name = f"**{certainty}**"
+            
+            embed_value = ""
+            
+            for index, row in df_certainty.iterrows():
+                
+                embed_value += f"•{row['cr_name']} <@{row['discord_id']}> {row['match_ratio']}\n"
+            
+            embed.add_field(name=embed_name, value=embed_value, inline=False)
+        
+    await ctx.response.send_message(embed=embed)
+
+@tree.command(name="attacks", description="Identify GDC players", guild=discord.Object(id=SERVER_ID))
+async def attacks(ctx):
+    min_ratio = 0.8
+    
+    guild = client.get_guild(SERVER_ID)
+    members = guild.members
+    
+    user_info = []
+    
+    for member in members:
+        user_info.append({
+            "name": member.name,
+            "discord_id": member.id,
+            "nickname": member.nick,
+            "global_name": member.global_name,
+            "discord_name": member.nick or member.global_name or member.name,
+        })
+    
+    df_discord_data = pd.DataFrame(user_info)
+    
+    RoyaleAPI_scraper = ScrapingRoyaleAPI("UURJ9CG", df_discord_data)
+    
+    df_players_data = RoyaleAPI_scraper.run()
+    
+    df_players_data = df_players_data.astype(str)
+    
+    df_players_data.to_csv("data/result.csv")
+    
+    embed = discord.Embed(title="War | "+str(RoyaleAPI_scraper.day), colour=discord.Colour(0x3e038c))
+        
+    clan_stats = [
+        f"<:sign:913172154269442048> **{RoyaleAPI_scraper.clan_name}**",
+        f"<:medals:1017445552859906148> **{RoyaleAPI_scraper.medals}**",
+        f"<:decksremaining:1017445543108165713> **{RoyaleAPI_scraper.decks_remaining}**",
+        f"<:slots:1017445562779435169> **{RoyaleAPI_scraper.slots}**",
+        ]
+    
+    embed.add_field(name=f"**Remaining Attacks**", value='\n'.join(clan_stats), inline=False)
+
+    for i in reversed(range(0, 3)):
+        
+        df_i_attacks = df_players_data[df_players_data["decks_used_today"] == str(i)]
+        
+        if len(df_i_attacks) >= 1:
+            
+            embed_name = f"**{4-i} Attacks**"
+            
+            embed_value = ""
+            
+            for index, row in df_i_attacks.iterrows():
+                
+                if float(row["match_ratio"]) >= min_ratio:
+                    embed_value += f"•{row['cr_name']} <@{row['discord_id']}>\n"
+                else:
+                    embed_value += f"•{row['cr_name']}\n"
+            
+            embed.add_field(name=embed_name, value=embed_value, inline=False)
+        
+    await ctx.response.send_message(embed=embed)
+    
+client.run(TOKEN)
---
+++ royaleapi_scraping_class.py
@@ -0,0 +1,119 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import pandas as pd
+import Levenshtein
+    
+def calculate_match_percentage(name1, name2):
+    max_length = max(len(name1), len(name2))
+    distance = Levenshtein.distance(name1, name2)
+    similarity = 1 - (distance / max_length)
+    return round(similarity, 3)
+
+class ScrapingRoyaleAPI:
+    def __init__(self, clan_id, df_discord_data):
+        
+        self.session=HTMLSession()
+        
+        self.clan_id = clan_id
+        
+        self.df_discord_data = df_discord_data
+        
+    def run(self):
+
+        self.get_soup()
+        
+        self.get_clan_data()
+        
+        self.get_players_data()
+        
+        self.get_players_discords_matches()
+        
+        return(self.df_players_data)
+    
+    def get_soup(self):
+        
+        r = self.session.get(f'https://royaleapi.com/clan/{self.clan_id}/war/race')
+        
+        text = r.text
+        
+        self.soup = BeautifulSoup(text,'html.parser')
+     
+    def get_clan_data(self):
+        
+        self.day = self.soup.find_all('div',class_="day")[0].get_text().strip('\n')
+        
+        self.medals = self.soup.select("#page_content > div.ui.attached.container.sidemargin0.content_container > div:nth-child(2) > div > a.clan.row.active_clan > div.outline.cw2__standing_outline > div.item.value.medal")[0].text.strip('\n')[2:]
+        
+        self.medals_avg = self.soup.select("#page_content > div.ui.attached.container.sidemargin0.content_container > div:nth-child(2) > div > a.clan.row.active_clan > div.outline.cw2__standing_outline > div.item.value.medal_avg")[0].text.strip('\n')
+        
+        self.decks_used_today = self.soup.select("#page_content > div.ui.attached.container.sidemargin0.content_container > div:nth-child(2) > div > a.clan.row.active_clan > div.outline.cw2__standing_outline > div.item.value.decks_used_today")[0].text.strip('\n')
+       
+        [nb_decks_used,nb_decks_max] = self.decks_used_today.replace(' ','').split('/')
+
+        self.decks_remaining = int(nb_decks_max) - int(nb_decks_used)
+        
+        self.clan_name = self.soup.select('#page_content > div:nth-child(5) > div.ui.attached.padded.segment > div.p_header_container > div:nth-child(1) > h1')[0].text.strip('\n')
+           
+    def get_players_data(self):
+        
+        self.df_players_data = pd.DataFrame(columns=["cr_id","cr_name","player_role","decks_used_today","decks_used","boat_attacks","fame","discord_name","discord_id","match_ratio"])
+        
+        players = self.soup.find_all('td',class_="player_name")
+        
+        for player in players:
+            
+            cr_id = '#'+player.find_all('a',href=True)[0]['href'].strip('/player/').strip('/battles')
+            
+            name = player.find_all('a',class_='player_name force_single_line_hidden')[0].get_text().strip('\n')
+            
+            player_role = player.find_all('div',class_='player_role')[0].get_text().strip('\n')
+
+            decks_used_today = player.find_all('div',class_='value_bg decks_used_today')[0].get_text().strip('\n')
+            
+            decks_used = player.find_all('div',class_='value_bg decks_used')[0].get_text().strip('\n')
+            
+            boat_attacks = player.find_all('div',class_='value_bg boat_attacks')[0].get_text().strip('\n')
+            
+            fame = player.find_all('div',class_='value_bg fame')[0].get_text().strip('\n')
+            
+            if player_role != "--":
+                
+                player_dict = {
+                    "cr_id":[cr_id],
+                    "cr_name":[name],
+                    "player_role":[player_role],
+                    "decks_used_today":[decks_used_today],
+                    "decks_used":[decks_used],
+                    "boat_attacks":[boat_attacks],
+                    "fame":[fame],
+                }
+                
+                self.df_players_data = pd.concat([self.df_players_data, pd.DataFrame.from_dict(player_dict)], ignore_index=True)
+
+        self.slots = len(self.df_players_data[self.df_players_data['decks_used_today'] == 0])
+
+    def get_players_discords_matches(self):
+        
+        print("Determining which discord ids are the best for these pseudos")
+
+        for index, player_row in self.df_players_data.iterrows():
+            cr_name = player_row["cr_name"]
+            best_match_name = ""
+            best_match_disc_id = ""
+            best_match_ratio = 0
+
+            for index, discord_row in self.df_discord_data.iterrows():
+                
+                disc_name = discord_row["discord_name"]
+                disc_id = discord_row["discord_id"]
+                
+                ratio = calculate_match_percentage(cr_name, disc_name)
+                
+                if ratio > best_match_ratio:
+                    best_match_ratio = ratio
+                    best_match_disc_name = disc_name
+                    best_match_disc_id = disc_id
+            
+            player_row["discord_name"] = best_match_disc_name
+            player_row["discord_id"] = best_match_disc_id 
+            player_row["match_ratio"] = best_match_ratio
